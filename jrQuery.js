82fae40b7129a6444edd0728f4c4841617ed812e
John Reese <jreese@plex.com>
2017-04-24 15:38:30 -0400

    Initial commit

---
+++ jrQuery.js
@@ -0,0 +1,48 @@
+﻿(function ()
+{
+  window.$ = function (selector)
+  {
+
+    if (selector.indexOf("#") > 0)
+    {
+      return new HtmlElement(document.getElementById(selector.substring(1)));
+    }
+    else
+    {
+      return new HtmlElementCollection(document.getElementsByClassName(selector.substring(1)));
+    }
+  }
+
+  var HtmlElement = function (element)
+  {
+
+    this.html = function ()
+    {
+      return element.innerHTML;
+    }
+
+    this.click = function (f)
+    {
+      element.onclick = function ()
+      {
+        f();
+      }
+    }
+  }
+
+  var HtmlElementCollection = function (elementArray)
+  {
+
+    this.click = function (f)
+    {
+      for (var x = 0; x < elementArray.length; x++)
+      {
+        elementArray[x].onclick = function ()
+        {
+          f(this);
+        }
+      }
+    }
+
+  }
+})();
