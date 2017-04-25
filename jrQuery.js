9dd169464452eeab2fee6d4aaabe9e51d93c2754
John Reese <jpreese@mtu.edu>
2017-04-24 23:04:34 -0400

    Add length function

--- jrQuery.js
+++ jrQuery.js
@@ -1,42 +1,38 @@
-﻿(function ()
-{
-  window.jrQuery = function (selector)
-  {
-    if (selector.indexOf("#") >= 0) {
-      return new HtmlElement(document.getElementById(selector.substring(1)));
-    }
-    else {
-      return new HtmlElementCollection(document.getElementsByClassName(selector.substring(1)));
-    }
-  }
+﻿(function () {
 
-  var HtmlElement = function (element)
-  {
-    this.html = function ()
-    {
-      return element.innerHTML;
-    }
-
-    this.click = function (f)
-    {
-      element.onclick = function ()
-      {
-        f();
-      }
-    }
-  }
-
-  var HtmlElementCollection = function (elementArray)
-  {
-    this.click = function (f)
-    {
-      for (var x = 0; x < elementArray.length; x++)
-      {
-        elementArray[x].onclick = function ()
-        {
-          f(this);
+    window.jrQuery = function(selector) {
+        if (selector.indexOf("#") >= 0) {
+            return new HtmlElement(document.getElementById(selector.substring(1)));
+        } else {
+            return new HtmlElementCollection(document.getElementsByClassName(selector.substring(1)));
         }
-      }
-    }
-  }
+    };
+
+    var HtmlElement = function(element) {
+        this.html = function() {
+            return element.innerHTML;
+        };
+
+        this.click = function(f) {
+            element.onclick = function() {
+                f();
+            };
+        };
+    };
+
+    var HtmlElementCollection = function(elementArray) {
+
+        this.click = function(f) {
+            for (var x = 0; x < elementArray.length; x++) {
+                elementArray[x].onclick = function() {
+                    f(new HtmlElement(this));
+                };
+            }
+        };
+
+        this.length = function() {
+            return elementArray.length;
+        };
+    };
+
 })();
