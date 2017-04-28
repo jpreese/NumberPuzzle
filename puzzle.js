026414ae1cfad64611c46e913b27acac305bad9c
John Reese <jpreese@mtu.edu>
2017-04-27 22:22:50 -0400

    Make helper methods private

--- puzzle.js
+++ puzzle.js
@@ -1,103 +1,106 @@
  (function($) {
 
-    window.Puzzle = function () { 
-        this.moves = 0;
-    };
+    window.Puzzle = function () {};
     
     window.Puzzle.prototype = {
 
-        tryMoveUp: function(e, position) {
-            var previousRow = e.parent().prev();
-            if (previousRow !== null && this.isEmptyCell(previousRow.children().eq(position))) {
-                this.moveNumber(e, previousRow.children().eq(position));
-                return true;
-            }
-        },
-
-        tryMoveLeft: function (e, position) {
-            var currentRow = e.parent().children();
-            var leftCell = currentRow.eq(position - 1);
-            if (this.isEmptyCell(leftCell)) {
-                this.moveNumber(e, leftCell);
-                return true;
-            }
-        },
-
-        tryMoveDown: function (e, position) {
-            var nextRow = e.parent().next();
-            if (nextRow !== null && this.isEmptyCell(nextRow.children().eq(position))) {
-                this.moveNumber(e, nextRow.children().eq(position));
-                return true;
-            }
-        },
-
-        tryMoveRight: function (e, position) {
-            var currentRow = e.parent().children();
-            var rightCell = currentRow.eq(position + 1);
-            if (this.isEmptyCell(rightCell)) {
-                this.moveNumber(e, rightCell);
-                return true;
-            }
-        },
-
-        doMoveIfValid: function (e) {
-            var currentPosition = e.parent().children().index(e);
-
-            if (this.tryMoveUp(e, currentPosition)) {
-                return true;
-            }
-
-            if (this.tryMoveLeft(e, currentPosition)) {
-                return true;
-            }
-
-            if (this.tryMoveDown(e, currentPosition)) {
-                return true;
-            }
-
-            if (this.tryMoveRight(e, currentPosition)) {
-                return true;
-            }
-
-            return false;
-        },
-
         tryMove: function(e) {
 
-            if(this.doMoveIfValid(e)) {
-                this.updateMoveCounter();
-                this.checkWinningCondition();
+            if(doMoveIfValid(e)) {
+                updateMoveCounter();
+                checkWinningCondition();
             }
 
-        },
+        }
 
-        isEmptyCell: function(e) {
-            return e !== null && e.html() === "";
-        },
+    };
 
-        moveNumber: function(source, destination) {
-            var sourceValue = source.html();
-            source.text("");
-            destination.text(sourceValue);
+    var moves = 0;
 
-            if (this.checkWinningCondition()) {
-                alert("won");
-            }
-        },
-
-        checkWinningCondition: function() {
-            var cells = $(".numberCell");
-            for (var x = 0; x < cells.length() ; x++) {
-                if (x+1 != cells.eq(x).html() && x+1 != cells.length()) {
-                    return false;
-                }
-            }
+    var tryMoveUp = function(e, position) {
+        var previousRow = e.parent().prev();
+        if (previousRow !== null && isEmptyCell(previousRow.children().eq(position))) {
+            moveNumber(e, previousRow.children().eq(position));
             return true;
-        },
-
-        updateMoveCounter: function() {
-            this.moves++;
-            $("#moves").text(this.moves);
         }
     };
+
+    var tryMoveLeft = function (e, position) {
+        var currentRow = e.parent().children();
+        var leftCell = currentRow.eq(position - 1);
+        if (isEmptyCell(leftCell)) {
+            moveNumber(e, leftCell);
+            return true;
+        }
+    };
+
+    var tryMoveDown = function (e, position) {
+        var nextRow = e.parent().next();
+        if (nextRow !== null && isEmptyCell(nextRow.children().eq(position))) {
+            moveNumber(e, nextRow.children().eq(position));
+            return true;
+        }
+    };
+
+    var tryMoveRight = function (e, position) {
+        var currentRow = e.parent().children();
+        var rightCell = currentRow.eq(position + 1);
+        if (isEmptyCell(rightCell)) {
+            moveNumber(e, rightCell);
+            return true;
+        }
+    };
+
+    var doMoveIfValid = function (e) {
+        var currentPosition = e.parent().children().index(e);
+
+        if (tryMoveUp(e, currentPosition)) {
+            return true;
+        }
+
+        if (tryMoveLeft(e, currentPosition)) {
+            return true;
+        }
+
+        if (tryMoveDown(e, currentPosition)) {
+            return true;
+        }
+
+        if (tryMoveRight(e, currentPosition)) {
+            return true;
+        }
+
+        return false;
+    };
+
+
+    var isEmptyCell = function(e) {
+        return e !== null && e.html() === "";
+    };
+
+    var moveNumber = function(source, destination) {
+        var sourceValue = source.html();
+        source.text("");
+        destination.text(sourceValue);
+
+        if (checkWinningCondition()) {
+            alert("won");
+        }
+    };
+
+    var checkWinningCondition = function() {
+        var cells = $(".numberCell");
+        for (var x = 0; x < cells.length() ; x++) {
+            if (x+1 != cells.eq(x).html() && x+1 != cells.length()) {
+                return false;
+            }
+        }
+        return true;
+    };
+
+    var updateMoveCounter = function() {
+        moves++;
+        $("#moves").text(moves);
+    };
+    
 })(jrQuery);
