04016e4efe9b877d07977c5b13da5e2b82efe81a
John Reese <jpreese@mtu.edu>
2017-04-30 17:26:29 -0400

    Resolve spacing discrepancies

--- scripts/notification.js
+++ scripts/notification.js
@@ -2,35 +2,35 @@
 
     var _ = window.Notification;
 
-	_.winNotification = function() {
-		var success = document.getElementById("success");
-		removeHiddenFromElement(success);
-	};
+    _.winNotification = function() {
+        var success = document.getElementById("success");
+        removeHiddenFromElement(success);
+    };
 
-	_.invalidNotification = function() {
-		var invalid = document.getElementById("invalid");
-		removeHiddenFromElement(invalid);
-		setTimeout(function() { fadeOutElement(invalid) }, 2000);
-	};
+    _.invalidNotification = function() {
+        var invalid = document.getElementById("invalid");
+        removeHiddenFromElement(invalid);
+        setTimeout(function() { fadeOutElement(invalid) }, 2000);
+    };
 
-	_.hideNotifications = function() {
-		var success = document.getElementById("success");
-		var invalid = document.getElementById("invalid");
-		addHiddenToElement(success);
-		addHiddenToElement(invalid);
-	};
+    _.hideNotifications = function() {
+        var success = document.getElementById("success");
+        var invalid = document.getElementById("invalid");
+        addHiddenToElement(success);
+        addHiddenToElement(invalid);
+    };
 
-	var addHiddenToElement = function(element) {
-		element.className += " hidden"
-	};
+    var addHiddenToElement = function(element) {
+        element.className += " hidden"
+    };
 
-	var removeHiddenFromElement = function(element) {
-		element.className = element.className.replace(/(?:^|\s)hidden(?!\S)/g , '');
-		element.style.display = "block";
-		element.style.opacity = 1;
-	};
+    var removeHiddenFromElement = function(element) {
+        element.className = element.className.replace(/(?:^|\s)hidden(?!\S)/g , '');
+        element.style.display = "block";
+        element.style.opacity = 1;
+    };
 
-	var fadeOutElement = function(element) {
+    var fadeOutElement = function(element) {
         element.style.opacity = 1;
 
         (function fade() {
